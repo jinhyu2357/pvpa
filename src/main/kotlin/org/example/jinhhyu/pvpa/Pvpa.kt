f80fb33d0c23361a03657e29495f5766403c26d5
jinhyu2357 <jinhyu2357@gmail.com>
2026-02-26 02:28:21 +0900

    Initial commit

---
+++ src/main/kotlin/org/example/jinhhyu/pvpa/Pvpa.kt
@@ -0,0 +1,14 @@
+package org.example.jinhhyu.pvpa
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Pvpa : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
